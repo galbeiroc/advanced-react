a3c4d37506578de7d110e79a97519e8955824435
galbeiroc <alcresgu_1989@hotmail.com>
2021-10-31 11:10:17 -0500

    use nodemon instead of pm2

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './lib/components/Index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        exclude: /node_modules/,
       }
     ],
   },
